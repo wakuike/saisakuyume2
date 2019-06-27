338eb68ab0c1dfa0ccdf2065da441419a57007fa
shoma <shoma.aptx4869@gmail.com>
2019-06-27 22:44:25 +0900

    コミット

---
+++ sisakuyume2/sisakuyume2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sisakuyume2
+//
+//  Created by 池内将真 on 2019/06/27.
+//  Copyright © 2019 WAKUike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
