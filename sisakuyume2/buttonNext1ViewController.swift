239fec38d0e26c6426a4643da882ce32cbbd5de8
yumeto <yumeto59@gmail.com>
2019-07-07 05:19:59 +0900

    猫ちゃん

---
+++ sisakuyume2/buttonNext1ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  buttonNext1ViewController.swift
+//  sisakuyume2
+//
+//  Created by 涌井夢人 on 2019/07/07.
+//  Copyright © 2019 WAKUike. All rights reserved.
+//
+
+import UIKit
+
+class buttonNext1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
